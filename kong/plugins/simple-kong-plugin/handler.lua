126475a7c8c65b95461a4990f986bb35d7a8df07
zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>
2022-04-12 12:22:22 +0700

    add custom environment variable in plugin
    Signed-off-by: zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>

--- kong/plugins/simple-kong-plugin/handler.lua
+++ kong/plugins/simple-kong-plugin/handler.lua
@@ -9,14 +9,10 @@
 -- The handlers are based on the OpenResty handlers, see the OpenResty docs for details
 -- on when exactly they are invoked and what limitations each handler has.
 ---------------------------------------------------------------------------------------------
-
-
-
 local plugin = {
   PRIORITY = 1000, -- set the plugin priority, which determines plugin execution order
   VERSION = "0.1", -- version in X.Y.Z format. Check hybrid-mode compatibility requirements.
 }
-
 
 
 -- do initialization here, any module level code runs in the 'init_by_lua_block',
@@ -60,23 +56,29 @@ function plugin:rewrite(plugin_conf)
 
 end --]]
 
-
+local owner = os.getenv("SIMPLE_KONG_PLUGIN_OWNER")
+local company = os.getenv("SIMPLE_KONG_PLUGIN_COMPANY")
 
 -- runs in the 'access_by_lua_block'
 function plugin:access(plugin_conf)
 
   -- your custom code here
   kong.log.inspect(plugin_conf)   -- check the logs for a pretty-printed config!
-  kong.service.request.set_header(plugin_conf.request_header, "this is on a request")
+
+  local value = "this is on the request, owned by " .. owner .. " from " .. company
+
+  kong.service.request.set_header(plugin_conf.request_header, value)
 
 end --]]
 
 
 -- runs in the 'header_filter_by_lua_block'
 function plugin:header_filter(plugin_conf)
+  kong.log.inspect(plugin_conf)
 
-  -- your custom code here, for example;
-  kong.response.set_header(plugin_conf.response_header, "this is on the response")
+  local value = "this is on the response, owned by " .. owner .. " from " .. company
+
+  kong.response.set_header(plugin_conf.response_header, value)
 
 end --]]
 
